584fed620804a6f8da875b08a55e6dfed233bb22
Dan Jackson <danjackson10095@hotmail.co.uk>
2017-04-14 18:56:12 +0100

    Initial test and controller.

---
+++ Nasa.PlutoRover/Nasa.PlutoRover.API.Tests/Controllers/PositionControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nasa.PlutoRover.API.Controllers;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Nasa.PlutoRover.API.Tests
+{
+	[TestClass]
+	public class PositionControllerTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+			var controller = new PositionController();
+			var result = controller.GetCurrentPosition() as OkNegotiatedContentResult<object>;
+
+			Assert.IsNotNull(result);
+
+		}
+	}
+}
---
+++ Nasa.PlutoRover/Nasa.PlutoRover.API/Controllers/PositionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Nasa.PlutoRover.API.Controllers
+{
+    public class PositionController : ApiController
+    {
+
+		public IHttpActionResult GetCurrentPosition()
+		{
+			return Ok(new object());
+		}
+
+    }
+}
